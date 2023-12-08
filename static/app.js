85892eb3111bfd9a393acc23ebe0e44b9fe65287
akintola4 <akintope830@gmail.com>
2023-12-08 02:31:50 +0100

    added a fav icon

--- static/app.js
+++ static/app.js
@@ -279,14 +279,6 @@ window.onload = function() {
         dialog.close();
     });
 
-   // Grab HTML Elements
-   const btn = document.querySelector("button.mobile-menu-button");
-   const menu = document.querySelector(".mobile-menu");
-
-   // Add Event Listeners
-   btn.addEventListener("click", () => {
-       menu.classList.toggle("hidden");
-   });
     //to start using the libary
     AOS.init(
     );
